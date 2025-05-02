3ed53c911076b12b54af56c3e5279fc1c3845f7e
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-05-02 12:56:08 -0600

    Added baseline scoring tests

--- refactored_notebook/scoring.py
+++ refactored_notebook/scoring.py
@@ -1,6 +1,6 @@
-from typing import Literal
-
 import numpy as np
+
+from refactored_notebook.data_models import ForecastType, ResolutionType
 
 
 def calculate_spot_peer_score(
@@ -11,28 +11,42 @@ def calculate_spot_peer_score(
 
 
 def calculate_spot_baseline_score(
-    forecast: list[float] | None, # binary: [p_yes, p_no], multiple choice: [p_a, p_b, p_c], numeric: [p_0, p_1, p_2, ...]
-    resolution: bool | str | float | None, # binary: bool, multiple choice: str, numeric: float
-    options: list[str] | None,
-    range_min: float | None,
-    range_max: float | None,
-    question_weight: float,
+    forecast: ForecastType,
+    resolution: ResolutionType,
+    options: list[str] | None = None,
+    range_min: float | None = None,
+    range_max: float | None = None,
+    question_weight: float = 1.0,
 ) -> float:
     """
     Question type can be infered from resolution type
+    Scoring math: https://www.metaculus.com/help/scores-faq/#What:~:text=given%20score%20type.-,What%20is%20the%20Baseline%20score%3F,-The%20Baseline%20score
     """
 
+
+    is_binary = isinstance(resolution, bool)
+    is_multiple_choice = isinstance(resolution, str)
+    is_numeric = isinstance(resolution, float) or isinstance(resolution, int)
+
+
     if forecast is None or resolution is None:
-        raise NotImplementedError("Havent decided how to handle null forecasts and resolutions")
+        raise NotImplementedError(
+            "Havent decided how to handle null forecasts or anulled resolutions"
+        )
 
     if len(forecast) == 0:
         raise ValueError("Forecast is empty")
 
-    baseline_score = None
+    if not is_numeric and any(p <= 0 or p >= 1 for p in forecast):
+        # @Check: Is it valid to have a numeric forecast with 0 probability for a number?
+        raise ValueError("Forecast contains probabilities outside of 0 to 1 range")
 
-    if isinstance(resolution, bool):
-        if len(forecast) != 1 or len(forecast) != 2:
-            raise ValueError("Binary questions must have exactly one forecast and two options (for yes or 'yes and no')")
+
+    if is_binary:
+        if len(forecast) != 1 and len(forecast) != 2:
+            raise ValueError(
+                "Binary questions must have exactly one or two forecasts (for yes or 'yes and no')"
+            )
 
         forecast_val = float(forecast[0])
         baseline_prob = 0.5
@@ -40,7 +54,7 @@ def calculate_spot_baseline_score(
             prob_for_resolution = forecast_val
         else:
             prob_for_resolution = 1 - forecast_val
-    elif isinstance(resolution, str):
+    elif is_multiple_choice:
         if options is None:
             raise ValueError("Options are required for multiple choice questions")
 
@@ -52,32 +66,47 @@ def calculate_spot_baseline_score(
         resolution_idx = options.index(str(resolution))
         prob_for_resolution = pmf[resolution_idx]
         baseline_prob = 1 / len(pmf)
-    elif isinstance(resolution, float):
+    elif is_numeric:
         if range_min is None or range_max is None:
-            raise ValueError("Range min and range max are required for numeric questions")
+            raise ValueError(
+                "Range min and range max are required for numeric questions"
+            )
+        if len(forecast) != 201:
+            raise ValueError("CDF should have 201 bins")
+        previous_prob = 0
+        for current_prob in forecast:
+            if current_prob < previous_prob:
+                raise ValueError("CDF should be in increasing order")
+            previous_prob = current_prob
 
         cdf = [float(p) for p in forecast]
-        pmf = [cdf[0]] + [cdf[i] - cdf[i-1] for i in range(1, len(cdf))] # @Ben check: is this a correct conversion?
+        pmf = [cdf[0]] + [
+            cdf[i] - cdf[i - 1] for i in range(1, len(cdf))
+        ]  # @Check: is this a correct conversion?
         pmf.append(1 - cdf[-1])
 
         resolution = float(resolution)
 
         bin_edges = np.linspace(range_min, range_max, 200)
-        resolution_idx = np.searchsorted(bin_edges, resolution, side='right')
+        resolution_idx = np.searchsorted(bin_edges, resolution, side="right")
 
         if resolution_idx >= len(pmf):
             raise ValueError("Resolution is out of bounds")
 
         prob_for_resolution = pmf[resolution_idx]
-        baseline_prob = 1 / len(pmf)  # bins = 201 because of extra appended bin
+        baseline_prob = 1 / len(pmf)  # bins = 201 because of extra appended bin # @Check: This comment seems off since its the cdf that has 201 bins
 
     else:
         raise ValueError("Unknown question type")
 
     if prob_for_resolution <= 0 or baseline_prob <= 0:
-        raise ValueError("Probability for resolution or baseline probability is less than or equal to 0 which could cause a log(0) issue")
+        raise ValueError(
+            "Probability for resolution or baseline probability is less than or equal to 0 which could cause a log(0) issue"
+        )
 
-    baseline_score = np.log2(prob_for_resolution / baseline_prob)
+    baseline_score = np.log2(
+        prob_for_resolution / baseline_prob
+    ) * 100    # @Check: check correctness (also shouldn't this be natural log?)
 
     if isinstance(resolution, float):
         baseline_score /= 2  # Numeric scores are halved
--- tests/generate_test_data.py
+++
@@ -1,5 +0,0 @@
-from refactored_notebook.data_models import User, Question, Forecast, Score
-
-
-# TODO: Things to test:
-# - peer rankings
