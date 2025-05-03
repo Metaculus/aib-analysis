a7e7d5e4c9bd39e663a85fea0c024fc33949c2fd
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-05-03 04:35:49 -0600

    Added some comments

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -10,6 +10,7 @@ from refactored_notebook.data_models import User, Question, Forecast, Score
 #    - make sure to determine the bot team only by the bot-only questions
 #    - make sure best bot team is decided by baseline score comparison to each other
 #    - make sure best bots for bot team are decided by lower bound of t test
+#    - make sure that worse bots come out on bottom
 # - Confidence interval code is wrong
 #   - make sure that there are large intervals if only a few forecasts, and small intervals if many forecasts
 #   - make sure bootstrap and t tests indicate the same things generally
