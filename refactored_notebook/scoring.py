2cf9c6fae825036c6c125bb62b227a5ab3dc1f69
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-05-07 06:30:16 -0600

    Got calculate_all_peer_scores working

--- refactored_notebook/scoring.py
+++ refactored_notebook/scoring.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from enum import Enum
+from typing import Literal
 
 import numpy as np
 from scipy.stats.mstats import gmean
@@ -6,21 +7,29 @@ from scipy.stats.mstats import gmean
 from refactored_notebook.data_models import ForecastType, ResolutionType
 
 
+class QuestionType(Enum):
+    BINARY = "binary"
+    MULTIPLE_CHOICE = "multiple_choice"
+    NUMERIC = "numeric"
+
 def calculate_peer_score(
     forecast: ForecastType,
     forecast_for_other_users: list[ForecastType],
     resolution: ResolutionType,
+    q_type: Literal["binary", "multiple_choice", "numeric"] | None = None,
     options: list[str] | None = None,
     range_min: float | None = None,
     range_max: float | None = None,
     question_weight: float = 1.0,
 ) -> float:
+    question_type = _determine_question_type(q_type, resolution)
+    resolution = _normalize_resolution(question_type, resolution, range_min, range_max)
     forecast_for_resolution = _determine_probability_for_resolution(
-        forecast, resolution, options, range_min, range_max
+        question_type, forecast, resolution, options, range_min, range_max
     )
     other_user_forecasts = [
         _determine_probability_for_resolution(
-            forecast, resolution, options, range_min, range_max
+            question_type, forecast, resolution, options, range_min, range_max
         )
         for forecast in forecast_for_other_users
     ]
@@ -32,43 +41,10 @@ def calculate_peer_score(
     return peer_score * question_weight
 
 
-def nominal_location_to_cdf_location(
-    nominal_location: float,
-    range_min: float,
-    range_max: float,
-    zero_point: float | None = None,
-) -> float:
-    """
-    Takes a location in nominal format (e.g. 123, "123", or datetime in iso format) and scales it to
-    metaculus's "internal representation" range [0, 1] incorporating question scaling
-    0.8 would incidate the nomial locatoin is at cdf index 201 * 0.8
-    Values higher/lower than 0 and 1 are resolutions that are above/below the upper/lower bound
-    """
-    assert isinstance(zero_point, float | None)
-
-    # TODO: Make sure to use datetime.fromisoformat(nominal_location).timestamp() if you start using date questions
-    scaled_location = float(nominal_location)
-
-    # Unscale the value to put it into the range [0,1]
-    if zero_point is not None:
-        # logarithmically scaled question
-        deriv_ratio = (range_max - zero_point) / (range_min - zero_point)
-        unscaled_location = (
-            np.log(
-                (scaled_location - range_min) * (deriv_ratio - 1)
-                + (range_max - range_min)
-            )
-            - np.log(range_max - range_min)
-        ) / np.log(deriv_ratio)
-    else:
-        # linearly scaled question
-        unscaled_location = (scaled_location - range_min) / (range_max - range_min)
-    return unscaled_location
-
-
 def calculate_baseline_score(
     forecast: ForecastType,
     resolution: ResolutionType,
+    q_type: Literal["binary", "multiple_choice", "numeric"] | None = None,
     options: list[str] | None = None,
     range_min: float | None = None,
     range_max: float | None = None,
@@ -80,13 +56,15 @@ def calculate_baseline_score(
     Question type can be infered from resolution type
     Scoring math: https://www.metaculus.com/help/scores-faq/#What:~:text=given%20score%20type.-,What%20is%20the%20Baseline%20score%3F,-The%20Baseline%20score
     """
+    question_type = _determine_question_type(q_type, resolution)
+    resolution = _normalize_resolution(question_type, resolution, range_min, range_max)
     prob_for_resolution = _determine_probability_for_resolution(
-        forecast, resolution, options, range_min, range_max
+        question_type, forecast, resolution, options, range_min, range_max
     )
     baseline_prob = _determine_baseline(
-        resolution, options, range_min, range_max, open_upper_bound, open_lower_bound
+        question_type, resolution, options, range_min, range_max, open_upper_bound, open_lower_bound
     )
-    divisor = _determine_divisor_for_baseline_score(resolution, options)
+    divisor = _determine_divisor_for_baseline_score(question_type, options)
     if prob_for_resolution <= 0 or baseline_prob <= 0:
         raise ValueError(
             "Probability for resolution or baseline probability is less than or equal to 0 which could cause a log(0) issue"
@@ -100,6 +78,7 @@ def calculate_baseline_score(
 
 
 def _determine_baseline(
+    question_type: QuestionType,
     resolution: ResolutionType,
     options: list[str] | None = None,
     range_min: float | None = None,
@@ -107,21 +86,22 @@ def _determine_baseline(
     open_upper_bound: bool | None = None,
     open_lower_bound: bool | None = None,
 ) -> float:
-    is_binary = isinstance(resolution, bool)
-    is_multiple_choice = isinstance(resolution, str)
-    is_numeric = isinstance(resolution, float) or isinstance(resolution, int)
-
-    if is_binary:
+    resolution = _normalize_resolution(question_type, resolution, range_min, range_max)
+    if question_type == QuestionType.BINARY:
         baseline_prob = 0.5
-    elif is_multiple_choice:
+    elif question_type == QuestionType.MULTIPLE_CHOICE:
         if options is None:
             raise ValueError("Options are required for multiple choice questions")
         baseline_prob = 1 / len(options)
-    elif is_numeric:
+    elif question_type == QuestionType.NUMERIC:
         if open_upper_bound is None or open_lower_bound is None:
             raise ValueError("Open upper bound and lower bound are required for numeric questions")
-        # @Check: Which version is correct?
+        if range_min is None or range_max is None:
+            raise ValueError("Range min and range max are required for numeric questions")
+        if not isinstance(resolution, float):
+            raise ValueError("Resolution must be a float for numeric questions")
 
+        # @Check: Which version is correct?
         # Version 1:
         resolved_outside_bounds = False
         assert range_min is not None and range_max is not None and resolution is not None, f"These need to be not None: Range min: {range_min}, range max: {range_max}, resolution: {resolution}"
@@ -156,6 +136,7 @@ def _determine_baseline(
 
 
 def _determine_probability_for_resolution(
+    q_type: QuestionType,
     forecast: ForecastType,
     resolution: ResolutionType,
     options: list[str] | None = None,
@@ -166,15 +147,7 @@ def _determine_probability_for_resolution(
     Returns a 0 to 1 probability for the resolution
     Also returns the baseline probability used in baseline scoring
     """
-
-    if resolution == "above_upper_bound" or resolution == "below_lower_bound":
-        raise ValueError(
-            "'above_upper_bound' or 'below_lower_bound' format not supported"
-        ) # This is an old resolution type in Q4 2024
-
-    is_numeric = isinstance(resolution, float) or isinstance(resolution, int)
-    is_binary = isinstance(resolution, bool)
-    is_multiple_choice = isinstance(resolution, str)
+    resolution = _normalize_resolution(q_type, resolution, range_min, range_max)
 
     if forecast is None or resolution is None:
         raise NotImplementedError(
@@ -184,18 +157,20 @@ def _determine_probability_for_resolution(
     if len(forecast) == 0:
         raise ValueError("Forecast is empty")
 
-    if not is_numeric and any(p <= 0 or p >= 1 for p in forecast):
+    if not q_type == QuestionType.NUMERIC and any(p <= 0 or p >= 1 for p in forecast):
         raise ValueError("Forecast contains probabilities outside of 0 to 1 range")
 
-    if is_binary:
+    if q_type == QuestionType.BINARY:
+        assert isinstance(resolution, bool)
         prob_for_resolution = _binary_resolution_prob(forecast, resolution)
-    elif is_multiple_choice:
+    elif q_type == QuestionType.MULTIPLE_CHOICE:
+        assert isinstance(resolution, str)
         if options is None:
             raise ValueError("Options are required for multiple choice questions")
         prob_for_resolution = _multiple_choice_resolution_prob(
             forecast, resolution, options
         )
-    elif is_numeric:
+    elif q_type == QuestionType.NUMERIC:
         if range_min is None or range_max is None:
             raise ValueError(
                 "Range min and range max are required for numeric questions"
@@ -278,19 +253,78 @@ def _numeric_resolution_prob(
 
 
 def _determine_divisor_for_baseline_score(
-    resolution: ResolutionType, options: list[str] | None = None
+    question_type: QuestionType, options: list[str] | None = None
 ) -> float:
-    is_binary = isinstance(resolution, bool)
-    is_multiple_choice = isinstance(resolution, str)
-    is_numeric = isinstance(resolution, float) or isinstance(resolution, int)
-
-    if is_binary:
+    if question_type == QuestionType.BINARY:
         return np.log(2)
-    elif is_multiple_choice:
+    elif question_type == QuestionType.MULTIPLE_CHOICE:
         if options is None:
             raise ValueError("Options are required for multiple choice questions")
         return np.log(len(options))
-    elif is_numeric:
+    elif question_type == QuestionType.NUMERIC:
         return 2
     else:
         raise ValueError("Unknown question type")
+
+def nominal_location_to_cdf_location(
+    nominal_location: float,
+    range_min: float,
+    range_max: float,
+    zero_point: float | None = None,
+) -> float:
+    """
+    Takes a location in nominal format (e.g. 123, "123", or datetime in iso format) and scales it to
+    metaculus's "internal representation" range [0, 1] incorporating question scaling
+    0.8 would incidate the nomial locatoin is at cdf index 201 * 0.8
+    Values higher/lower than 0 and 1 are resolutions that are above/below the upper/lower bound
+    """
+    assert isinstance(zero_point, float | None)
+
+    # TODO: Make sure to use datetime.fromisoformat(nominal_location).timestamp() if you start using date questions
+    scaled_location = float(nominal_location)
+
+    # Unscale the value to put it into the range [0,1]
+    if zero_point is not None:
+        # logarithmically scaled question
+        deriv_ratio = (range_max - zero_point) / (range_min - zero_point)
+        unscaled_location = (
+            np.log(
+                (scaled_location - range_min) * (deriv_ratio - 1)
+                + (range_max - range_min)
+            )
+            - np.log(range_max - range_min)
+        ) / np.log(deriv_ratio)
+    else:
+        # linearly scaled question
+        unscaled_location = (scaled_location - range_min) / (range_max - range_min)
+    return unscaled_location
+
+def _normalize_resolution(question_type: QuestionType, resolution: ResolutionType, range_min: float | None, range_max: float | None) -> ResolutionType:
+    if resolution == "annulled" or resolution == "ambiguous":
+        return None
+
+    if question_type == QuestionType.NUMERIC:
+        if range_min is None or range_max is None:
+            raise ValueError("Range min and range max are required for numeric questions")
+        if resolution == "above_upper_bound":
+            return range_max + 0.1
+        elif resolution == "below_lower_bound":
+            return range_min - 0.1
+        else:
+            return resolution
+    else:
+        return resolution
+
+
+def _determine_question_type(question_type: Literal["binary", "multiple_choice", "numeric"] | None, resolution: ResolutionType) -> QuestionType:
+    if question_type is None:
+        if isinstance(resolution, bool):
+            return QuestionType.BINARY
+        elif isinstance(resolution, float) or isinstance(resolution, int) or resolution == "above_upper_bound" or resolution == "below_lower_bound":
+            return QuestionType.NUMERIC
+        elif isinstance(resolution, str):
+            return QuestionType.MULTIPLE_CHOICE
+        else:
+            raise ValueError(f"Cannot infer question type from resolution. Please provide a question type. Resolution: {resolution}")
+    else:
+        return QuestionType(question_type)
