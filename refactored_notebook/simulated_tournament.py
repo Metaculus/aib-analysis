0f780c6bc6d173c46ab44584ece3cabda1683229
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-06-08 23:03:12 -0600

    Refactored tests and pro data now loading again

--- refactored_notebook/simulated_tournament.py
+++ refactored_notebook/simulated_tournament.py
@@ -51,42 +51,38 @@ class SimulatedTournament(BaseModel):
         assert self._spot_forecasts_cache is not None, "Spot forecasts cache is not initialized"
         return self._spot_forecasts_cache
 
-    def question_to_forecasts(self, question_id: int) -> list[Forecast]:
-        return [
+    def question_to_spot_forecasts(self, question_id: int) -> list[Forecast]:
+        spot_forecasts = [
             forecast
-            for forecast in self.forecasts
+            for forecast in self.spot_forecasts
             if forecast.question.question_id == question_id
         ]
-
-    def question_to_scores(self, question_id: int) -> list[Score]:
-        return [
-            score
-            for score in self.scores
-            if score.forecast.question.question_id == question_id
-        ]
+        return spot_forecasts
 
     def user_to_scores(self, user_name: str) -> list[Score]:
         return [score for score in self.scores if score.forecast.user.name == user_name]
 
-    def forecast_to_scores(self, forecast_id: str) -> list[Score]:
-        return [score for score in self.scores if score.forecast.id == forecast_id]
-
-    def user_question_type_to_scores(
-        self, user_name: str, question_id: int, score_type: ScoreType
-    ) -> list[Score]:
-        return [
+    def get_spot_score_for_question_and_user(
+        self, question_id: int, user_name: str, score_type: ScoreType
+    ) -> Score:
+        assert (
+            score_type.is_spot_score()
+        ), "Only spot scores are supported for this method"
+        scores = [
             score
             for score in self.scores
             if score.forecast.user.name == user_name
             and score.forecast.question.question_id == question_id
             and score.type == score_type
         ]
+        assert len(scores) == 1, "Expected exactly for question for user if spot score"
+        return scores[0]
 
 
     @model_validator(mode="after")
     def initialize_tournament(self) -> Self:
         logger.info("Initializing caches")
-        self._inialize_spot_forecast_cache()
+        self._initialize_spot_forecast_cache()
 
         self._user_cache = {
             forecast.user.name: forecast.user for forecast in self.forecasts
@@ -115,7 +111,7 @@ class SimulatedTournament(BaseModel):
         logger.info("Finished initializing scoring caches")
         return self
 
-    def _inialize_spot_forecast_cache(self) -> None:
+    def _initialize_spot_forecast_cache(self) -> None:
         spot_forecasts: dict[tuple[str, int], Forecast] = {}
         for forecast in self.forecasts:
             question = forecast.question
@@ -129,37 +125,17 @@ class SimulatedTournament(BaseModel):
                 spot_forecasts[key] = forecast
         self._spot_forecasts_cache = list(spot_forecasts.values())
 
-
-    def get_spot_score_for_question(
-        self, question_id: int, user_name: str, score_type: ScoreType
-    ) -> Score:
-        assert (
-            score_type.is_spot_score()
-        ), "Only spot scores are supported for this method"
-        scores = self.user_question_type_to_scores(user_name, question_id, score_type)
-        assert len(scores) == 1, "Expected exactly for question for user if spot score"
-        return scores[0]
-
-    def get_forecasters_on_question(self, question_id: int) -> list[User]:
-        unique_user_names = set(
-            forecast.user.name
-            for forecast in self.question_to_forecasts(question_id)
-        )
-        users = [self._user_cache[name] for name in unique_user_names]
-        return users
-
     def _calculate_spot_scores_for_forecast(
         self, forecast_to_score: Forecast
     ) -> list[Score]:
-        assert (
-            forecast_to_score in self._spot_forecasts_cache
-        ), "Forecast to score must be in spot forecasts cache"
+        if forecast_to_score not in self._spot_forecasts_cache:
+            raise ValueError("Forecast to score must be in spot forecasts cache")
         resolution = forecast_to_score.question.resolution
         if isinstance(resolution, AmbiguousResolutionType):
             return []
 
         spot_forecasts_from_others: list[Forecast] = (
-            self.question_to_forecasts(forecast_to_score.question.question_id)
+            self.question_to_spot_forecasts(forecast_to_score.question.question_id)
         )
         spot_forecasts_from_others.remove(forecast_to_score)
         spot_peer_score = forecast_to_score.get_spot_peer_score(
@@ -169,4 +145,3 @@ class SimulatedTournament(BaseModel):
 
         scores = [spot_peer_score, spot_baseline_score]
         return scores
-
--- tests/test_process_tournament.py
+++ tests/test_process_tournament.py
@@ -1,14 +1,16 @@
+import logging
+
 import pytest
 
 from refactored_notebook.custom_types import ScoreType, UserType
-from refactored_notebook.load_tournament_data import load_tournament
+from refactored_notebook.load_tournament import load_tournament
 from refactored_notebook.simulated_tournament import SimulatedTournament
 from tests.mock_data_maker import (
     make_forecast,
     make_question_binary,
     make_user,
 )
-import logging
+from refactored_notebook.process_tournament import get_leaderboard
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +25,7 @@ def test_simulated_tournament_leaderboard_ranking_good_forecast_higher(score_typ
     forecast_medium = make_forecast(question, user_medium, [0.5])
     forecast_bad = make_forecast(question, user_bad, [0.1])
     tournament = SimulatedTournament(forecasts=[forecast_good, forecast_medium, forecast_bad])
-    leaderboard = tournament.get_leaderboard(score_type)
+    leaderboard = get_leaderboard(tournament, score_type)
     assert leaderboard.entries[0].user.name == "good"
     assert leaderboard.entries[1].user.name == "medium"
     assert leaderboard.entries[2].user.name == "bad"
@@ -31,25 +33,5 @@ def test_simulated_tournament_leaderboard_ranking_good_forecast_higher(score_typ
 
 def test_simulated_tournament_leaderboard_ranking_runs(pro_tournament: SimulatedTournament):
     tournament = pro_tournament
-    leaderboard = tournament.get_leaderboard(ScoreType.SPOT_PEER)
+    leaderboard = get_leaderboard(tournament, ScoreType.SPOT_PEER)
     assert len(leaderboard.entries) == 10
-
-def test_scores_are_calculated_from_pro_tournament(pro_tournament: SimulatedTournament):
-    validate_scores_calculated_from_simulated_tournament(pro_tournament)
-
-def test_scores_are_calculated_from_bot_tournament(bot_tournament: SimulatedTournament):
-    validate_scores_calculated_from_simulated_tournament(bot_tournament)
-
-def validate_scores_calculated_from_simulated_tournament(tournament: SimulatedTournament):
-    # This test is mostly just running data validation.
-    logger.info(f"Tournament has {len(tournament.questions)} questions")
-    for question in tournament.questions:
-        forecasters_on_question = tournament.get_forecasters_on_question(question.question_id)
-        for user in forecasters_on_question:
-            for score_type in ScoreType:
-                if question.resolution is None:
-                    continue
-
-                score = tournament.get_spot_score_for_question(question.question_id, user.name, score_type)
-                assert score.type == score_type
-
