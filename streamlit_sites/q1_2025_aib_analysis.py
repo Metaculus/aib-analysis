b592d8fa7705bbeea8029f9d46ec0f1b386289e0
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-06-16 21:45:56 -0600

    Added q1_2025 streamlit entry point

---
+++ streamlit_sites/q1_2025_aib_analysis.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+top_level_dir = os.path.abspath(os.path.join(current_dir, "../"))
+sys.path.append(top_level_dir)
+
+from aib_analysis.main import main
+
+if __name__ == "__main__":
+    main()
