fc1a57373e8b0dc78c4d3f7aa8cb534991bd1544
Ben Wilson <wilson.benjamin1000@gmail.com>
2025-10-14 07:59:02 -0600

    Found q3 control vs bots to be 4.39

---
+++ filter_for_control_bots.py
@@ -0,0 +1,25 @@
+"""
+Changes the forecast csv so there are only rows for the control bots.
+"""
+import pandas as pd
+
+control_bots = [
+    "mf-bot-1",
+    "mf-bot-3",
+]
+def main() -> None:
+    input_file = "local/q3_archive/bots_score_data_q3.csv"
+    pro_file = "local/q3_archive/pros_score_data_q3.csv"
+    output_file = "local/bots_score_data_q3_control.csv"
+
+    pro_question_titles: list[str] = list(pd.read_csv(pro_file)["question_title"].unique())
+
+    df = pd.read_csv(input_file)
+    df = df[df["forecaster"].isin(control_bots)]
+    df = df[df["question_title"].isin(pro_question_titles)]
+    df.to_csv(output_file, index=False)
+
+    pass
+
+if __name__ == "__main__":
+    main()
